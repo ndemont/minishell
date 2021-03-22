0b1ef87a1173a918dee184a3910ce5cd63ef85ba
Nastasia DEMONT <ndemont@e2r7p17.42.fr>
2021-03-22 17:51:13 +0100

    Correct: display with colors

--- srcs/display_prompt.c
+++ srcs/display_prompt.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/22 16:02:15 by ndemont           #+#    #+#             */
-/*   Updated: 2021/03/22 17:36:02 by gpetit           ###   ########.fr       */
+/*   Updated: 2021/03/22 17:47:54 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	display_prompt(void)
 {
-	write(1, PURPLE, 10);
+	write(1, PURPLE, 7);
 	write(1, "minishellrose-v1$ ", 18);
    	write(1, RESET, 9);
 	return (1);
