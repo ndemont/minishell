fbef90f5391b1da01ee42dd9d124957787bdb7de
Nastasia Demont <nastasia@demont.me>
2021-03-25 21:21:36 +0100

    Update: echo.c option -n

--- srcs/echo.c
+++ srcs/echo.c
@@ -6,26 +6,11 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 18:31:28 by ndemont           #+#    #+#             */
-/*   Updated: 2021/03/25 19:13:06 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/03/25 21:21:19 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void		free_split(char **split)
-{
-	int i;
-
-	if (!split)
-		return ;
-	i = 0;
-	while (split[i])
-	{
-		free(split[i]);
-		i++;
-	}
-	free(split);
-}
 
 static char	*ft_env(int *i, char *str) /*add the general structure to the arguments*/
 {
@@ -38,6 +23,8 @@ static char	*ft_env(int *i, char *str) /*add the general structure to the argume
 	while (str[count] != ' ')
 		count++;
 	new = malloc(sizeof(char) * (count + 1));
+	if (!new)
+		return(0);
 	new[count] = 0;
 	j = 0;
 	while (*i < count)
@@ -56,9 +43,12 @@ static char	*ft_env(int *i, char *str) /*add the general structure to the argume
 static char	*ft_quote_mark(int i, char *str) /*add the general structure to the arguments*/
 {
 	char	*new;
+	char	*var;
+	char	*tmp;
 
 	while (str[i] && str[i] != "\"")
 	{
+		tmp = new;
 		if (str[i] != "$")
 		{
 			new = ft_strjoin(new, (char *)str[i]);
@@ -66,50 +56,66 @@ static char	*ft_quote_mark(int i, char *str) /*add the general structure to the 
 		}
 		else
 		{
+			var = ft_env(&i, str);
+			if (!var)
+				return (0);
 			new = ft_strjoin(new, ft_env(&i, str));
 		}
+		free(tmp);
+		if (!new)
+			return (0);
 	}
 	if (str[i] == '\0')
 		return (0);
 	return (new);
 }
 
-int			ft_echo(char *str, char *echo) /*add the general structure to the arguments*/
+static int	ft_parse_echo(char *arg, char *echo, int i)
 {
-	int		i;
-	int		option;
-	char		*new;
-	char		*tmp;
-
-	i = 0;
-	option = 0;
-	while (str[i] == ' ')
-		i++;
-	if (str[i] == '-' && str[i + 1] == 'n')
-		option = 1;
-	while (str[i])
+	char *tmp;
+	
+	while (arg[i])
 	{
 		tmp = echo;
-		while (str[i] == ' ')
+		while (arg[i] == ' ')
 			i++;
-		if (str[i] == '"')
+		if (arg[i] == '"')
 		{
-			new = ft_quote_mark(i, str);
+			new = ft_quote_mark(i, arg);
 			echo = ft_strjoin(echo, new);
 			free(new);
 		}
-		else if (str[i] == '$')
+		else if (arg[i] == '$')
 		{
-			new = ft_env(i, str);
+			new = ft_env(i, arg);
 			echo = ft_strjoin(echo, new);
 			free(new);
 		}
 		else
-			echo = ft_strjoin(echo, (char *)str[i]);
+			echo = ft_strjoin(echo, (char *)arg[i]);
 		free(tmp);
 		i++;
 	}
-	free_split(split);
+	
+}
+
+int			ft_echo(char *arg, char *echo) /*add the general structure to the arguments*/
+{
+	int		i;
+	int		option;
+	char	*tmp;
+
+	i = 0;
+	option = 0;
+	while (arg[i] == ' ')
+		i++;
+	if (arg[i] == '-' && arg[i + 1] == 'n')
+		option = 1;
+	ft_parse_echo(arg, echo, i);
 	if (!option)
+	{
+		tmp = echo;
 		echo = ft_strjoin(echo, "\n");
+		free(tmp);
+	}		
 }
