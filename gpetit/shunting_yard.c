95af9e00079cb58aea5a25ed761fed7283fb19ac
gpetit <gpetit@student.42.fr>
2021-03-29 18:51:56 +0200

    reversed Polish notation OK

--- gpetit/shunting_yard.c
+++ gpetit/shunting_yard.c
@@ -3,10 +3,13 @@
 
 int		rank(char c)
 {
-	if (c = '+' || c = '-')
+	if (c == '+' || c == '-')
 		return (1);
-	else if (c = '*' || c = '/')
+	else if (c == '*' || c == '/')
 		return (2);
+	else
+		return (-1);
+}
 
 void	push(char c, t_list **stack)
 {
@@ -15,27 +18,25 @@ void	push(char c, t_list **stack)
 
 	content = (char*)malloc(sizeof(char));
 	*content = c;
+	new = ft_lstnew(content);
 	if (!(*stack))
-		*stack = ft_lstnew(content);
+		*stack = new;
 	else
-	{
-		new = ft_lstnew(content);
 		ft_lstadd_front(stack, new);
-	}
 }
 
-void	pop(char **polish, t_list **stack)
+void	pop(char *polish, t_list **stack)
 {
 	t_list *tmp;
 
-	*polish = (*stack)->content;
+	*polish = *(char *)(*stack)->content;
 	tmp = *stack;
-	stack = (*stack)->next;
+	*stack = (*stack)->next;
 	free(tmp->content);
 	free(tmp);
 }
 
-void	queue(char **polish, char c)
+void	queue(char *polish, char c)
 {
 	*polish = c;
 }
@@ -52,24 +53,27 @@ char	*reverse_polish(char *str)
 	{
 		if (ft_isdigit(str[i]))
 			queue(&(polish[j++]), str[i]);
-		else if (ft_strchr(str[i], "+-*/"))
+		else if (ft_strchr( "+-*/", str[i]))
 		{
-			if (stack && rank(str[i]) < rank(stack->content))
+			if (stack && rank(str[i]) < rank(*(char *)stack->content))
 			{
-				pop(&(polish[j++]), stack);
-				push(str[i], stack);
+				printf("1 : %c\n", str[i]);
+				while (stack && rank(str[i]) < rank(*(char *)stack->content))
+					pop(&(polish[j++]), &stack);
+				push(str[i], &stack);
 			}
 			else
-				push(str[i], stack);
+			{
+				printf("2 : %c\n", str[i]);
+				push(str[i], &stack);
+			}
 		}
 		i++;
 	}
 	while (stack)
-	{
-		pop(&(polish[j++]), stack);
-		stack = stack->next;
-	}
+		pop(&(polish[j++]), &stack);
 	polish[j] = '\0';
+	return (polish);
 }
 
 void	shunting_yard(char *str)
