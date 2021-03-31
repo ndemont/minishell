4f894e894fb5b23bb7618fdb763b112706ac3d70
Nastasia DEMONT <ndemont@e2r6p17.42.fr>
2021-03-31 12:09:25 +0200

    It worrrrks parsing arg for each buildin

--- srcs/buildin_parser.c
+++ srcs/buildin_parser.c
@@ -6,18 +6,36 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/29 15:58:24 by ndemont           #+#    #+#             */
-/*   Updated: 2021/03/30 12:34:43 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/03/31 12:06:48 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*get_arg(char *input)
+char	*get_arg(char *input, int *i)
 {
-	char *arg;
+	char	*arg;
+	int 	j;
 
 	arg = 0;
-	(void)input;
+	while (input[*i] && input[*i] == ' ')
+		*i = *i + 1;
+	j = *i;
+	while (input[*i] && input[*i] != ' ' && input[*i] != '\'' && input[*i] != '"')
+		*i = *i + 1;
+	if (input[*i] == '\'')
+	{
+		*i = *i + 1;
+		while (input[*i] != '\'')
+			*i = *i + 1;
+	}
+	else if (input[*i] == '"')
+	{
+		*i = *i + 1;
+		while (input[*i] != '"')
+			*i = *i + 1;
+	}
+	arg = ft_substr(input, j, *i - j);
 	return (arg);
 }
 
@@ -56,18 +74,44 @@ int	count_arg(char *input)
 
 void	get_buildin(t_node *token)
 {
-	char *list[8] = {"echo", "export", "env", "cd", "pwd", "unset", "exit", "$?"};
-	int i;
-	int count;
+	char	*list[8] = {"echo", "export", "env", "cd", "pwd", "unset", "exit", "$?"};
+	int		i;
+	int		j;
+	int		count;
 
 	(void)list;
 	i = 0;
+	j = 0;
 	printf("token input = [%s]\n", token->input);
 	count = count_arg(token->input);
-	printf("count = %i\n", count);
+	//printf("count = %i\n", count);
+	if (count > 1)
+	{
+		token->arg = (char **)malloc(sizeof(char) * count);
+		token->arg[count] = 0;
+	}
 	while (token->input[i])
 	{
+		if (!i)
+			token->builtin = get_arg(token->input, &i);
+		else
+		{
+			token->arg[j] = get_arg(token->input, &i);
+			j++;
+		}
 		i++;
+	}
+	printf("Builtin = [%s]\n", token->builtin);
+	if (token->arg)
+	{
+		i = 0;
+		printf("Args =");
+		while (token->arg[i])
+		{
+			printf("[%s]  ", token->arg[i]);
+			i++;
+		}
+		printf("\n");
 	}
 }
 
@@ -78,10 +122,8 @@ t_node	**ft_buldin_parser(t_node **token_tab)
 	i = 0;
 	while (token_tab[i])
 	{
-		printf("type = %i\n", token_tab[i]->type);
 		if (!token_tab[i]->type)
 			get_buildin(token_tab[i]);
-		printf("loop = %i\n", i);
 		i++;
 	}
 	return (token_tab);
