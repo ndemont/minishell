8ccba37aa8378ba3bfada2f951f695b050fa6d8d
Nastasia Demont <nastasia@demont.me>
2021-04-08 17:46:43 +0200

    Update: echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,46 +6,53 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 18:31:28 by ndemont           #+#    #+#             */
-/*   Updated: 2021/04/08 14:44:13 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/08 17:45:51 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <string.h>
 
-char		*ft_echo_cat(char **arg, int i)
+char		*ft_echo_cat(char **arg, int *i)
 {
-	int len;
-	char *ret;
+	int		len;
+	char	*tmp;
+	char	*ret;
 
-	while (arg[i] && arg[i + 1])
+	ret = (char *)malloc(sizeof(char));
+	ret[0] = 0;
+	while (arg[*i] && arg[*i + 1])
 	{
-		len = ft_strlen(arg[i]);
-		ret = (char *)malloc(sizeof(char) * (len + 2));
+		len = ft_strlen(arg[*i]);
+		tmp = ret;
+		ret = (char *)malloc(sizeof(char) * (len + 2 + ft_strlen(tmp)));
 		if (!ret)
 			print_errors(strerror(errno));
-		ret = strcpy(ret, arg[i]);
-		ret[len] = ' ';
-		ret[len + 1] = '\0';
-		i++;
+		ret = ft_strjoin(tmp, arg[*i]);
+		ret[len + ft_strlen(tmp)] = ' ';
+		ret[len + 1 + ft_strlen(tmp)] = '\0';
+		*i = *i + 1;
+		free(tmp);
 	}
 	return (ret);
 }
 
-char 		*ft_echo_catlast(char *ret, char **arg, int i, int flag)
+char 		*ft_echo_catlast(char *ret, char **arg, int *i, int flag)
 {
 	int	len;
 	char *tmp;
 
-	len = ft_strlen(arg[i]);
-	tmp = (char *)malloc(sizeof(char) * (len + 1 + flag));
-	if (!tmp)
+	len = ft_strlen(arg[*i]);
+	tmp = ret;
+	ret = (char *)malloc(sizeof(char) * (len + ft_strlen(tmp) + 1 + flag));
+	if (!ret)
 		print_errors(strerror(errno));
-	tmp = strcpy(ret, arg[i]);
+	ret = ft_strjoin(tmp, arg[*i]);
 	if (flag)
-		tmp[len] = '\n';
-	tmp[len + flag] = '\0';
-	return (tmp);
+		ret[len + ft_strlen(tmp)] = '\n';
+	ret[len + ft_strlen(tmp)+ flag] = '\0';
+	free(tmp);
+	return (ret);
 }
 
 int			ft_echo(char **arg)
@@ -54,18 +61,16 @@ int			ft_echo(char **arg)
 	int i;
 	char *ret;
 
-	(void)datas;
 	flag = 1;
-	if (!arg[0])
-		return (0);
 	i = 1;
 	if (!(ft_strcmp(arg[i], "-n")))
 	{
 		flag = 0;
 		i++;
 	}
-	ret = ft_echo_cat(arg, i); //PENSER A FREE
-	ret = ft_echo_catlast(ret, arg, i, flag); //NAS, PENSE A FREE STP
+	ret = ft_echo_cat(arg, &i); //PENSER A FREE
+	ret = ft_echo_catlast(ret, arg, &i, flag); //NAS, PENSE A FREE STP
+	//printf("ret 2 = [%s]\n", ret);
 	ft_putstr_fd(ret, STDOUT_FILENO);
 	return (1);
 }
