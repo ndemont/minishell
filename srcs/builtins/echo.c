9d55a3b249bae35fb31400aa726c5375284cfe96
Nastasia Demont <nastasia@demont.me>
2021-04-08 17:52:26 +0200

    Update: echo works (except -n

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,11 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 18:31:28 by ndemont           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/04/08 17:45:51 by ndemont          ###   ########.fr       */
-=======
-/*   Updated: 2021/04/08 14:44:13 by ndemont          ###   ########.fr       */
->>>>>>> bec5a084cbcce4052da667fa13b8aed596037b98
+/*   Updated: 2021/04/08 17:51:41 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +34,7 @@ char		*ft_echo_cat(char **arg, int *i)
 		*i = *i + 1;
 		free(tmp);
 	}
-<<<<<<< HEAD
 	return (ret);
-=======
-	return (1);
->>>>>>> bec5a084cbcce4052da667fa13b8aed596037b98
 }
 
 char 		*ft_echo_catlast(char *ret, char **arg, int *i, int flag)
@@ -57,28 +49,18 @@ char 		*ft_echo_catlast(char *ret, char **arg, int *i, int flag)
 		print_errors(strerror(errno));
 	ret = ft_strjoin(tmp, arg[*i]);
 	if (flag)
-<<<<<<< HEAD
 		ret[len + ft_strlen(tmp)] = '\n';
 	ret[len + ft_strlen(tmp)+ flag] = '\0';
 	free(tmp);
 	return (ret);
-=======
-		builtin->ret[len] = '\n';
-	builtin->ret[len + flag] = '\0';
-	return (1);
->>>>>>> bec5a084cbcce4052da667fa13b8aed596037b98
 }
 
 int			ft_echo(char **arg)
 {
 	int flag;
 	int i;
-<<<<<<< HEAD
 	char *ret;
-=======
->>>>>>> bec5a084cbcce4052da667fa13b8aed596037b98
 
-	(void)datas;
 	flag = 1;
 	i = 1;
 	if (!(ft_strcmp(arg[i], "-n")))
