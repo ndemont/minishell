512b4b670ec72c3ba06d849e020765ad10393772
Nastasia Demont <nastasia@demont.me>
2021-04-08 11:34:21 +0200

    Update: error management in echo.c

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,12 +6,43 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 18:31:28 by ndemont           #+#    #+#             */
-/*   Updated: 2021/04/08 11:22:53 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/08 11:33:38 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include <string.h>
+
+int			ft_echo_cat(t_node *builtin, int i)
+{
+	int len;
+
+	while (builtin->arg[i] && builtin->arg[i + 1])
+	{
+		len = ft_strlen(builtin->arg[i]);
+		builtin->ret = (char *)malloc(sizeof(char) * (len + 2));
+		if (!builtin->ret)
+			print_errors(strerror(errno));
+		builtin->ret = strcpy(builtin->ret, builtin->arg[i]);
+		builtin->ret[len] = ' ';
+		builtin->ret[len + 1] = '\0';
+		i++;
+	}
+}
+
+int			ft_echo_catlast(t_node *builtin, int i, int flag)
+{
+	int	len;
+
+	len = ft_strlen(builtin->arg[i]);
+	builtin->ret = (char *)malloc(sizeof(char) * (len + 1 + flag));
+	if (!builtin->ret)
+		print_errors(strerror(errno));
+	builtin->ret = strcpy(builtin->ret, builtin->arg[i]);
+	if (flag)
+		builtin->ret[len] = '\n';
+	builtin->ret[len + flag] = '\0';
+}
 
 int			ft_echo(t_big *datas, t_node *builtin)
 {
@@ -19,29 +50,16 @@ int			ft_echo(t_big *datas, t_node *builtin)
 	int i;
 	int len;
 
-	flag = 0;
+	flag = 1;
 	if (!builtin->arg[0])
 		return (0);
 	i = 1;
 	if (!(ft_strcmp(builtin->arg[i], "-n")))
 	{
-		flag = 1;
+		flag = 0;
 		i++;
 	}
-	while (builtin->arg[i] && builtin->arg[i + 1])
-	{
-		len = ft_strlen(builtin->arg[i]);
-		builtin->ret = (char *)malloc(sizeof(char) * (len + 2));
-		builtin->ret = strcpy(builtin->ret, builtin->arg[i]);
-		builtin->ret[len] = ' ';
-		builtin->ret[len] = '\0';
-		i++;
-	}
-	len = ft_strlen(builtin->arg[i]);
-	builtin->ret = (char *)malloc(sizeof(char) * (len + 1 + flag));
-	builtin->ret = strcpy(builtin->ret, builtin->arg[i]);
-	if (flag)
-		builtin->ret[len] = '\n';
-	builtin->ret[len] = '\0';
+	ft_echo_cat(builtin, i);
+	ft_echo_catlast(builtin, i, flag);
 	return (1);
 }
