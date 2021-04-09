b6a729010b6b885a27bb6c9107c923465ed5b00c
gpetit <gpetit@student.42.fr>
2021-04-09 19:21:40 +0200

    env now stored in an array list

--- srcs/main.c
+++ srcs/main.c
@@ -1,35 +1,17 @@
 #include "minishell.h"
 
-void	store_env(char **env, t_big *datas)
-{
-	int k;
-
-	k = 0;
-	while (env[k])
-		k++;
-	datas->env = (char **)malloc(sizeof(char *) * (k + 1));
-	k = 0;
-	while (env[k])
-	{
-		datas->env[k] = ft_strdup(env[k]);
-		k++;
-	}
-	datas->env[k] = NULL;
-}
-
 int		main(int ac, char **av, char **env)
 {
 	t_big	datas;
-	int		i;
 
 	(void)ac;
 	(void)av;
 	store_env(env, &datas);
-	i = 0;
-	while (datas.env[i])
+	while(datas.env)
 	{
-		printf("%s\n", datas.env[i]);
-		i++;
+		printf("VAR = %s || ", ((t_var *)datas.env->content)->var);
+		printf("VALUE = %s\n", ((t_var *)datas.env->content)->value);
+		datas.env = datas.env->next;
 	}
 	while (read_input(&datas));
 	return (0);
