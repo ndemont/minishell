87edff2e6f944ce7c31b0c70428ea21b3cebe796
Nastasia Demont <nastasia@demont.me>
2021-04-15 14:09:43 +0200

    Move pipe part in binary to grammar/pipe

--- srcs/binaries.c
+++ srcs/binaries.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 13:31:47 by gpetit            #+#    #+#             */
-/*   Updated: 2021/04/15 14:08:04 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/15 14:09:16 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,33 +131,6 @@ void	exec_binary(char *command, char **argv)
 		write(1, ": command not found\n", 20);
 	}
 	free_double(cmd);
-}
-
-void	exec_piped_cmd(char *command, char **argv, int is_built_in, t_big *datas)
-{
-	int fd[2];
-	pid_t pid1;
-
-	datas->flag_pipe = 1;
-	pipe(fd);
-	pid1 = fork();
-	if (pid1 == 0)
-	{
-		dup2(datas->fd, STDIN_FILENO);
-		dup2(fd[1], STDOUT_FILENO);
-		close(fd[1]);
-		close(fd[0]);
-		if (is_built_in == 1)
-			exec_built_in(command, argv, datas);
-		else
-			exec_binary(command, argv);
-		close(datas->fd);
-		exit(0);  //permet de fermer execve dans le fork après l'avoir RUN
-	}
-	waitpid(pid1, NULL, 0);
-	dup2(fd[0], datas->fd);
-	close(fd[1]);
-	close(fd[0]);
 }
 
 void	execute_tree(t_node *root, int n, t_big *datas)
---
+++ srcs/grammar/pipe.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/15 14:08:58 by ndemont           #+#    #+#             */
+/*   Updated: 2021/04/15 14:09:17 by ndemont          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	exec_piped_cmd(char *command, char **argv, int is_built_in, t_big *datas)
+{
+	int fd[2];
+	pid_t pid1;
+
+	datas->flag_pipe = 1;
+	pipe(fd);
+	pid1 = fork();
+	if (pid1 == 0)
+	{
+		dup2(datas->fd, STDIN_FILENO);
+		dup2(fd[1], STDOUT_FILENO);
+		close(fd[1]);
+		close(fd[0]);
+		if (is_built_in == 1)
+			exec_built_in(command, argv, datas);
+		else
+			exec_binary(command, argv);
+		close(datas->fd);
+		exit(0);  //permet de fermer execve dans le fork après l'avoir RUN
+	}
+	waitpid(pid1, NULL, 0);
+	dup2(fd[0], datas->fd);
+	close(fd[1]);
+	close(fd[0]);
+}
