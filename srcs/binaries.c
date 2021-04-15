ef27cb73ac98678fe8def66dc9a76a54ffe62f29
Nastasia Demont <nastasia@demont.me>
2021-04-15 14:08:35 +0200

    Move semicolon part in binary to grammar/semicolon

--- srcs/binaries.c
+++ srcs/binaries.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 13:31:47 by gpetit            #+#    #+#             */
-/*   Updated: 2021/04/14 17:56:15 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/15 14:08:04 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -158,42 +158,6 @@ void	exec_piped_cmd(char *command, char **argv, int is_built_in, t_big *datas)
 	dup2(fd[0], datas->fd);
 	close(fd[1]);
 	close(fd[0]);
-}
-
-void	exec_semicolon_cmd(char *command, char **argv, int is_built_in, t_big *datas)
-{
-	int fd[2];
-	pid_t pid1;
-
-	(void)argv;
-	(void)is_built_in;
-	(void)command;
-	datas->flag_pipe = 0;
-	if (is_built_in == 1)
-		exec_built_in(command, argv, datas);
-	else if (is_built_in == 2)
-		print_std(datas->fd);
-	else if (is_built_in == 0)
-	{
-		pipe(fd);
-		pid1 = fork();
-		if (pid1 == 0)
-		{
-			dup2(datas->fd, STDIN_FILENO);
-			dup2(fd[1], STDOUT_FILENO);
-			close(fd[1]);
-			close(fd[0]);
-			exec_binary(command, argv);
-			close(datas->fd);
-			exit(0);  //permet de fermer execve dans le fork après l'avoir RUN
-		}
-		waitpid(pid1, NULL, 0);
-		dup2(fd[0], datas->fd);
-		close(fd[1]);
-		close(fd[0]);
-	}
-	//print_std(datas->fd);
-	//datas->fd = dup(STDIN_FILENO);
 }
 
 void	execute_tree(t_node *root, int n, t_big *datas)
