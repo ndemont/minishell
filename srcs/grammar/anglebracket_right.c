72fcac40d18810da323b37af8d4e36f85b3ea6d1
Nastasia Demont <nastasia@demont.me>
2021-04-16 17:46:53 +0200

    Add multiple redirections

--- srcs/grammar/anglebracket_right.c
+++ srcs/grammar/anglebracket_right.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 14:10:15 by ndemont           #+#    #+#             */
-/*   Updated: 2021/04/15 15:49:38 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/16 17:44:16 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,25 +22,28 @@ void	print_std_fd(int fd_in, int fd_out)
 		ft_putstr_fd(line, fd_out);
 		ft_putstr_fd("\n", fd_out);
 		free(line);
+		line = 0;
 	}
 	ft_putstr_fd(line, fd_out);
 	free(line);
+	line = 0;
 }
 
-void	exec_anglebracket_right(char **argv, t_big *datas)
+void	exec_anglebracket_right(char **argv, t_big *datas, t_node *last)
 {
 	int fd;
 
 	datas->flag_pipe = 0;
 	fd = open(argv[0], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	print_std_fd(datas->fd, fd);
+	if (!last)
+		print_std_fd(datas->fd, fd);
 	close(fd);
 }
 
-void	redirections(int type, char **argv, t_big *datas)
+void	redirections(int type, char **argv, t_big *datas, t_node *last)
 {
 	if (type == 3)
-		exec_anglebracket_right(argv, datas);
+		exec_anglebracket_right(argv, datas, last);
 	//else if (type == 3)
 	//	append_file(str, path);
 	//else if (type == 4)
--- srcs/main.c
+++ srcs/main.c
@@ -1,11 +1,23 @@
 #include "minishell.h"
 
+void	init_data(t_big *datas)
+{
+	datas->fd = -1;
+	datas->flag_pipe = 0;
+	datas->flag_redirection = 0;
+	datas->env = 0;
+	datas->export = 0;
+	datas->hidden = 0;
+	datas->root = 0;
+}
+
 int		main(int ac, char **av, char **env)
 {
 	t_big	datas;
 
 	(void)ac;
 	(void)av;
+	init_data(&datas);
 	store_env(env, &datas);
 	while (read_input(&datas));
 	return (0);
