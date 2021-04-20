3250b53107e60f51c53b5284c110d65ae4ae9334
Nastasia Demont <nastasia@demont.me>
2021-04-20 20:48:45 +0200

    Clean redirections

--- srcs/ast/binaries.c
+++ srcs/ast/binaries.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 13:31:47 by gpetit            #+#    #+#             */
-/*   Updated: 2021/05/10 15:45:03 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/16 23:00:49 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ void	print_std(int fd)
 {
 	char	*line;
 	int 	ret;
-	
-	line = NULL;
+
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
 		ft_putstr(line);
@@ -28,7 +27,7 @@ void	print_std(int fd)
 	free(line);
 }
 
-char **path_array(char *command, t_list *env)
+char **path_array(char *command)
 {
 	char *tmp;
 	char **paths;
@@ -36,16 +35,12 @@ char **path_array(char *command, t_list *env)
 	int k;
 
 	k = 0;
-	paths = NULL;
-	while (env && ft_strcmp(((t_var *)(env->content))->var, "PATH"))
-		env = env->next;
-	if (!env)
+	if ((tmp = getenv("PATH")) == NULL)
 	{
 		perror("PATH is invalid ");
 		exit(0); //replace by custom exit fct that deallocate everything 
 	}
-	if (!ft_strcmp(((t_var *)(env->content))->var, "PATH"))
-		paths = ft_split(((t_var *)(env->content))->value, ':');
+	paths = ft_split(tmp, ':');
 	if (!paths)
 	{
 		perror("ft_split failed ");
@@ -91,113 +86,81 @@ char **mono_array(char *str)
 }
 
 
-char **build_array(char *command, t_list *env)
+char **build_array(char *command)
 {
 	char **cmd; 
 
 	if (ft_strchr(command, '/'))
 		cmd = mono_array(command);
 	else
-		cmd = path_array(command, env);
+		cmd = path_array(command);
 	return (cmd);
 }
 
-int		exec_built_in(char *command, char **argv, t_big *datas)
+void	exec_built_in(char *command, char **argv, t_big *datas)
 {
-	int ret;
-
 	if (!(ft_strcmp(command, "echo")))
-		ret = ft_echo(argv, datas);
+		ft_echo(argv, datas);
 	if (!(ft_strcmp(command, "cd")))
-		ret = ft_cd(argv, datas);
+		ft_cd(argv, datas);
 	if (!(ft_strcmp(command, "env")))
-		ret = ft_env(argv, datas);
+		ft_env(datas);
 	if (!(ft_strcmp(command, "pwd")))
-		ret = ft_pwd(datas);
+		ft_pwd(datas);
 	if (!(ft_strcmp(command, "export")))
-		ret = ft_export(argv, datas);
+		ft_export(argv, datas);
 	if (!(ft_strcmp(command, "unset")))
-		ret = ft_unset(argv, datas);
-	if (!(ft_strcmp(command, "exit")))
-		ret = ft_exit(argv, datas);
+		ft_unset(argv, datas);
 	if(ft_strchr(command, '='))
-		ret = ft_hidden(argv, datas);
-	return (ret);
+		ft_hidden(argv, datas);
+	return;
 }
 
-char	**build_array_env(t_list *lst)
-{
-	int size;
-	int i;
-	char *line;
-	char **env;
-
-	i = 0;
-	size = ft_lstsize(lst);
-	env = (char **)malloc(sizeof(char *) * (size + 1));
-	while (lst)
-	{
-		line = ft_strjoin(((t_var *)(lst->content))->var, "=");
-		line = ft_realloc(line, ft_strlen(line) + ft_strlen(((t_var *)(lst->content))->value) + 1);
-		ft_strlcat(line, ((t_var *)(lst->content))->value, (ft_strlen(line) + ft_strlen(((t_var *)(lst->content))->value) + 1));
-		env[i] = line;
-		i++;
-		lst = lst->next;
-	}
-	env[i] = 0;
-	return (env);
-}
-
-void	exec_binary(char *command, char **argv, t_big *datas)
+void	exec_binary(char *command, char **argv)
 {
 	char **cmd;
-	char **env;
 	int k;
 	int ret;
 
-	cmd = build_array(command, *datas->env);
-	env = build_array_env(*datas->env);
+	cmd = build_array(command);
  	k = 0;
-	while (cmd[k] && ((ret = execve(cmd[k], argv, env)) == -1))
+	while (cmd[k] && ((ret = execve(cmd[k], argv, NULL)) == -1))
 		k++;
 	if (ret == -1)
 	{
-		tcaps.ret = RET_ERROR;
-		printf("minishellrose: %s: command not found\n", command);
+		write(1, "minishellrose: ", 15);
+		ft_putstr(command);
+		write(1, ": command not found\n", 20);
 	}
 	free_double(cmd);
-	free_double(env);
 }
 
 void	execute_tree(t_node *root, int n, t_big *datas, int side)
 {
-	if (!datas->quit)
-	{
-		if (root->left)
-			execute_tree(root->left, root->type, datas, 1);
-		if (root->right)
-			execute_tree(root->right, root->type, datas, 2);
-		if (!datas->quit && n == 0 && root->command)
-			exec_semicolon_cmd(root->command, root->arg, 0, datas);
-		if (!datas->quit && n == 0 && root->builtin)
-			exec_built_in(root->builtin, root->arg, datas);
-		if (n == 1 && (root->command || root->builtin))
-			exec_piped_cmd(root->command, root->builtin, root->arg, datas);
-		if (n == 5 && root->command)
-			exec_semicolon_cmd(root->command, root->arg, 0, datas);
-		if (n == 5 && root->builtin)
-			exec_semicolon_cmd(root->builtin, root->arg, 1, datas);
-		else if (n == 5)
-			exec_semicolon_cmd(root->builtin, root->arg, 2, datas);
-		if ((n == 2 || n == 3) && side == 1 && (root->command || root->builtin))
-			redirections(n, root->arg, datas);
-		else if ((n == 2 || n == 3) && side == 2 && (root->command || root->builtin))
-			exec_piped_cmd(root->command, root->builtin, root->arg, datas);
-		else if (n == 4 && side == 1)
-			redirections(n, root->arg, datas);
-		else if (n == 4 && side == 2 && (root->builtin || root->command))
-			exec_piped_cmd(root->command, root->builtin, root->arg, datas);
-	}
+	if (root->left)
+		execute_tree(root->left, root->type, datas, 1);
+	if (root->right)
+		execute_tree(root->right, root->type, datas, 2);
+	if (n == 1 && root->command)
+		exec_piped_cmd(root->command, root->arg, 0, datas); //RAjOUTER FD_OUT
+	if (n == 1 && root->builtin)
+		exec_piped_cmd(root->builtin, root->arg, 1, datas);
+	if (n == 5 && root->command)
+		exec_semicolon_cmd(root->command, root->arg, 0, datas);
+	if (n == 5 && root->builtin)
+		exec_semicolon_cmd(root->builtin, root->arg, 1, datas);
+	else if (n == 5)
+		exec_semicolon_cmd(root->builtin, root->arg, 2, datas);
+	if (n == 0 && root->builtin)
+		exec_built_in(root->builtin, root->arg, datas);
+	if (n == 0 && root->command)
+		exec_piped_cmd(root->command, root->arg, 0, datas);
+	if ((n == 2 || n == 3) && side == 2)
+		redirections(n, root->arg, datas);
+	else if ((n == 2 || n == 3) && side == 1 && root->builtin)
+		exec_piped_cmd(root->builtin, root->arg, 1, datas);
+	else if ((n == 2 || n == 3) && side == 1 && root->command)
+		exec_piped_cmd(root->command, root->arg, 0, datas);
 }
 
 void	executions(t_big *datas)
@@ -210,12 +173,5 @@ void	executions(t_big *datas)
 	execute_tree(datas->root, 0, datas, 0);
 	if (datas->flag_pipe)
 		print_std(datas->fd);
-	if (datas->flag_bracket)
-	{
-		print_std_fd(datas->fd, datas->fd_out);
-		ft_putstr_fd("\n", datas->fd);
-		close(datas->fd_out);
-		datas->fd_out = STDOUT_FILENO;
-	}
-	close(datas->fd); //CAUTIOUS
+	close(datas->fd);
 }
--- srcs/grammar/redirection.c
+++ srcs/grammar/redirection.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 14:10:15 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/04 18:29:19 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/20 20:40:45 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	print_std_fd(int fd_in, int fd_out)
 {
 	char	*line;
-	int		ret;
+	int 	ret;
 
 	while ((ret = get_next_line(fd_in, &line)) > 0)
 	{
@@ -25,87 +25,49 @@ void	print_std_fd(int fd_in, int fd_out)
 		line = 0;
 	}
 	ft_putstr_fd(line, fd_out);
-	if (line)
-		free(line);
+	free(line);
 	line = 0;
-}
-
-void	ft_copy_arg(char **argv, t_big *datas)
-{
-	int		len_new;
-	int		len_old;
-	int		len_total;
-	char	**tmp;
-	
-	len_new = 1;
-	while (argv && argv[len_new])
-		len_new++;
-	len_new--;
-	if (!len_new)
-		return ;
-	len_old = 0;
-	while (datas->redirection_arg && datas->redirection_arg[len_old])
-		len_old++;
-	len_total  = len_old + len_new;
-	tmp = datas->redirection_arg;
-	datas->redirection_arg = (char **)malloc(sizeof(char *) * (len_total + 1));
-	datas->redirection_arg[len_total] = 0;
-	len_new = 0;
-	while (len_new < len_old)
-	{
-		datas->redirection_arg[len_new] = ft_strdup(tmp[len_new]);
-		len_new++;
-	}
-	len_new = 1;
-	while (len_old < len_total)
-	{
-		datas->redirection_arg[len_old] = ft_strdup(argv[len_new]);
-		len_new++;
-		len_old++;
-	}
-	if (tmp)
-		free(tmp);
 }
 
 void	exec_anglebracket_right(char **argv, t_big *datas)
 {
+	int fd;
+
 	datas->flag_pipe = 0;
-	datas->flag_left_bracket = 1;
-	if (datas->fd_out != STDOUT_FILENO)
-		close(datas->fd_out);
-	datas->fd_out = open(argv[0], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	ft_copy_arg(argv, datas);
-	datas->file_name = ft_strdup(argv[0]);
-	datas->flag_bracket = 1;
+	if (datas->flag_bracket == 0)
+	{
+		fd = open(argv[0], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		print_std_fd(datas->fd, fd);
+	}
+	else
+		fd = open(argv[0], O_CREAT | O_WRONLY, 0644);
+	close(fd);
 }
 
 void	exec_anglebracket_left(char **argv, t_big *datas)
 {
-	int	fd;
+	int fd;
 
 	datas->flag_pipe = 0;
-	printf("file name = %s\n", argv[0]);
-	fd = open(argv[0], O_RDONLY);
-	printf("fd = %d\n", fd);
-	if (fd < 0)
+	if (datas->flag_bracket == 0)
 	{
-		printf("minishellrose: %s: No such file or directory\n", argv[0]);
-		datas->quit = 1;
+		fd = open(argv[0], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		print_std_fd(datas->fd, fd);
 	}
-	if (datas->flag_left_bracket == 0)
-		datas->fd = fd;
+	else
+		fd = open(argv[0], O_CREAT | O_WRONLY, 0644);
+	close(fd);
 }
 
 void	exec_double_anglebracket_right(char **argv, t_big *datas)
 {
+	int fd;
+
 	datas->flag_pipe = 0;
-	datas->flag_left_bracket = 1;
-	if (datas->fd_out != STDOUT_FILENO)
-		close(datas->fd_out);
-	datas->fd_out = open(argv[0], O_CREAT | O_WRONLY | O_APPEND, 0644);
-	ft_copy_arg(argv, datas);
-	datas->file_name = ft_strdup(argv[0]);
-	datas->flag_bracket = 1;
+	fd = open(argv[0], O_CREAT | O_WRONLY | O_APPEND, 0644);
+	if (datas->flag_bracket == 0)
+		print_std_fd(datas->fd, fd);
+	close(fd);
 }
 
 void	redirections(int type, char **argv, t_big *datas)
@@ -116,4 +78,6 @@ void	redirections(int type, char **argv, t_big *datas)
 		exec_anglebracket_right(argv, datas);
 	else if (type == 4)
 		exec_anglebracket_left(argv, datas);
+	datas->flag_bracket = 1;
+	write(datas->fd, "\0", 1);
 }
