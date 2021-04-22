d7d67c1089a8281d8f56e7725ee77351a1700061
Guillaume PETIT <gpetit@e2r4p2.42.fr>
2021-04-22 11:58:51 +0200

    Fixed: Segfault with few builtins, coming from Bad Struct init

--- includes/minishell.h
+++ includes/minishell.h
@@ -112,5 +112,6 @@ char		*get_env(t_big *data, char *var);
 int			cmp_list(t_var *lst, t_var *lst2);
 t_var		*fill_tmp(char *str);
 void		init_data(t_big *datas);
+void		actualize_data(t_big *datas);
 
 #endif
