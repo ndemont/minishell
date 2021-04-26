12e98ee3823c618693cd279bec13bbfbfc0978f7
Nastasia DEMONT <ndemont@e2r3p16.42.fr>
2021-04-26 17:03:40 +0200

    Fix history up and down

--- srcs/history/save_history.c
+++ srcs/history/save_history.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/23 20:36:46 by ndemont           #+#    #+#             */
-/*   Updated: 2021/04/26 14:30:30 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/26 16:19:22 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void	save_history(char *line, t_big *datas)
 
 	update_history_list(datas->history, line, 1);
 	test = *datas->history;
-	// while(test)
-	// {
-	// 	if (test->command)
-	// 		printf("[%s]\n", test->command);
-	// 	test = test->next;
-	// }
+	while(test)
+	{
+		if (test->command)
+			printf("[%s]\n", test->command);
+		test = test->next;
+	}
 }
