a68c17f468798acd3e553d1e07b92212746f6a40
Guillaume PETIT <gpetit@e2r3p18.42.fr>
2021-04-27 12:01:31 +0200

    En cours: counting lines when inputing history

--- srcs/history/browse_history.c
+++ srcs/history/browse_history.c
@@ -46,6 +46,22 @@ t_history	*browse_down_history(t_history *current, char **browse, int begin, cha
 	return (current);
 }
 
+void	lines_added(char *str)
+{
+	int size;
+	int line_len;
+
+	size = tcaps.c_max - tcaps.c_start;
+	line_len = ft_strlen(str);
+	if (line_len > size)
+	{
+		tcaps.line_lvl++;
+		line_len -= size;
+		size = tcaps.c_max;
+		tcaps.line_lvl += ((line_len / size) + (line_len % size));
+	}
+}
+
 void	browse_history(t_big *datas, char **line, int signal)
 {
 	static char			*browse;
@@ -63,6 +79,7 @@ void	browse_history(t_big *datas, char **line, int signal)
 		current = browse_down_history(current, &browse, datas->flag_history, input);
 	if (browse)
 	{
+		lines_added(browse);
 		ft_putstr_fd(browse, STDIN_FILENO);
 		*line = browse;
 	}
