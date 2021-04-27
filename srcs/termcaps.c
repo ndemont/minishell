de767037b65907a8427e8d1225d464082aabf44c
Nastasia DEMONT <ndemont@e2r3p16.42.fr>
2021-04-27 15:11:48 +0200

    Add EOT

--- srcs/termcaps.c
+++ srcs/termcaps.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 17:51:10 by gpetit            #+#    #+#             */
-/*   Updated: 2021/04/27 14:29:47 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/27 14:32:02 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,7 +138,7 @@ void	do_the_right_thing(int *i, char *buf, char **line, t_big *datas)
 	else if (buf[0] == 3)
 		write(0, "ETX\n", 4);
 	else if (buf[0] == 4)
-		end_of_transmission(datas, line);
+		end_of_transmission(datas, *line);
 	else if (buf[0] == 28)
 		write(0, "FS\n", 3);
 }
