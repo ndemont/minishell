a2f6274a35e10582e4572340855914b5c95d3204
Nastasia DEMONT <ndemont@e2r3p16.42.fr>
2021-04-27 15:11:48 +0200

    Add EOT

--- srcs/signals/eot.c
+++ srcs/signals/eot.c
@@ -6,17 +6,20 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 13:46:13 by ndemont           #+#    #+#             */
-/*   Updated: 2021/04/27 14:29:10 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/27 14:33:15 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void    end_of_transmission(t_big *datas)
+void    end_of_transmission(t_big *datas, char *line)
 {
  	(void)datas;
      //free_tokens(token_tab);
 	//free_datas(datas);
-    write(0, "exit\n", 5);
-    exit(1);
+    if (!line[0])
+    {
+        write(0, "exit\n", 5);
+        exit(1);
+    }
 }
