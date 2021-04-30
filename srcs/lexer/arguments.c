c80320e1ef367b94d42c629040d5a7248652a400
Nastasia DEMONT <ndemont@e3r3p18.42.fr>
2021-04-30 12:14:24 +0200

    Add malloc protection in lexer, first step

--- srcs/lexer/arguments.c
+++ srcs/lexer/arguments.c
@@ -6,77 +6,122 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/30 11:27:45 by ndemont           #+#    #+#             */
-/*   Updated: 2021/04/30 11:30:32 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/04/30 12:11:05 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+char		*get_backslash(char *input, int *i, int j)
+{
+	char *first_part;
+	char *second_part;
+	char *new;
+
+	new = 0;
+	if (input[*i] == '\\' && input[*i + 1])
+	{
+		*i = *i + 1;
+		first_part = ft_substr(input, j, *i - j - 1);
+		second_part = ft_substr(input, *i, 1);
+		if (!first_part || !second_part)
+			return (print_errors(strerror(errno)));
+		new = ft_strjoin(first_part, second_part);
+		if (!new)
+			return (print_errors(strerror(errno)));
+		free(first_part);
+		free(second_part);
+		*i = *i + 1;
+	}
+	else if (input[*i] == '\\' && !input[*i + 1])
+		print_errors("minishellrose: missing char at end of line");
+	return (new);
+}
+
+char		*get_variable(char *input, int *i, int j)
+{
+	char *tmp;
+	char *new;
+	
+	new = get_quote(input, i, &j);
+	if (!new)
+		return (print_errors(strerror(errno)));
+	if (new[0] == '$')
+	{
+		tmp = new;
+		new = ft_strjoin("\"", tmp);
+		if (!new)
+			return (print_errors(strerror(errno)));
+		free(tmp);
+		tmp = new;
+		new = ft_strjoin(new, "\"");
+		if (!new)
+			return (print_errors(strerror(errno)));
+		free(tmp);
+	}
+	return (new);
+}
+
+char		*get_str(char *input, int *i, int j)
+{
+	char *new;
+	char *tmp;
+	
+	new = ft_substr(input, j, *i - j);
+	if (!new)
+		return (print_errors(strerror(errno)));
+	if (new[0] == '$')
+	{
+		tmp = new;
+		new = ft_strjoin("\"", tmp);
+		if (!new)
+			return (print_errors(strerror(errno)));
+		free(tmp);
+		tmp = new;
+		new = ft_strjoin(new, "\"");
+		if (!new)
+			return (print_errors(strerror(errno)));
+		free(tmp);
+	}
+	return (new);
+}
+
 char		*get_arg(char *input, int *i)
 {
 	char	*arg;
-	char	*tmp1;
-	char	*tmp2;
-	char	*tmp3;
-	char	*tmp4;
+	char	*first_part;
+	char	*second_part;
 	int		j;
 
 	arg = malloc(sizeof(char));
+	if (!arg)
+		return (print_errors(strerror(errno)));
 	arg[0] = '\0';
 	while (input[*i] && (input[*i] == ' ' || input[*i] == '\t'))
 		*i = *i + 1;
 	while (input[*i] && input[*i] != ' ' && input[*i] != '\t')
 	{
 		j = *i;
+		first_part = arg;
 		while (input[*i] && input[*i] != ' ' && input[*i] != '\'' && input[*i] != '"' && input[*i] != '\t' && input[*i] != '\\')
 			*i = *i + 1;
-		tmp1 = arg;
-		if (input[*i] == '\\' && input[*i + 1])
-		{
-			*i = *i + 1;
-			tmp2 = ft_substr(input, j, *i - j - 1);
-			tmp4 = ft_substr(input, *i, 1);
-			tmp2 = ft_strjoin(tmp2, tmp4);
-			*i = *i + 1;
-		}
-		else if (input[*i] == '\\' && !input[*i + 1])
-			print_errors("minishellrose: missing char at end of line");
+		if (input[*i] == '\\')
+			second_part = get_backslash(input, i, j);
 		else if (input[j] == '\'')
 		{
-			tmp2 = get_quote(input, i, &j);
-			if (!tmp2)
+			second_part = get_quote(input, i, &j);
+			if (!second_part)
 				print_errors(strerror(errno));
 		}
 		else if (input[j] == '\"')
-		{
-			tmp2 = get_quote(input, i, &j);
-			if (!tmp2)
-				print_errors(strerror(errno));
-			if (tmp2[0] == '$')
-			{
-				tmp3 = tmp2;
-				tmp2 = malloc(sizeof(char) * (ft_strlen(tmp3) + 3));
-				tmp2 = ft_strjoin("\"", tmp3);
-				tmp2 = ft_strjoin(tmp2, "\"");
-				printf("tmp2 = [%s]\n", tmp2);
-			}
-		}
+			second_part = get_variable(input, i, j);
 		else
-		{
-			tmp2 = ft_substr(input, j, *i - j);
-			if (!tmp2)
-				print_errors(strerror(errno));
-			if (tmp2[0] == '$')
-			{
-				tmp3 = tmp2;
-				tmp2 = malloc(sizeof(char) * (ft_strlen(tmp3) + 3));
-				tmp2 = ft_strjoin("\"", tmp3);
-				tmp2 = ft_strjoin(tmp2, "\"");
-			}
-		}
-		arg = ft_strjoin(tmp1, tmp2);
-		free(tmp1);
-		free(tmp2);
+			second_part = get_str(input, i , j);
+		if (!second_part)
+			return (0);
+		arg = ft_strjoin(first_part, second_part);
+		free(first_part);
+		free(second_part);
 	}
 	return (arg);
 }
