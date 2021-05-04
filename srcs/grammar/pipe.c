49b8bb0a7db9b4520f5ac14890bf2dd99f85f5f6
Nastasia Demont <nastasia@demont.me>
2021-05-04 11:34:39 +0200

    Add modificiations in pipe after rebase

--- srcs/grammar/pipe.c
+++ srcs/grammar/pipe.c
@@ -6,22 +6,18 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 14:08:58 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/04 11:21:07 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/05/04 11:34:00 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< HEAD
 void	actualize_return_status(int ret_status)
 {
 	tcaps.ret = WEXITSTATUS(ret_status);
 }
 
-static void	exec_child(char *command, char **av, int is_built_in, t_big *datas)
-=======
-static void	exec_child(char *cmd, char *builtin, char **av, t_big *datas)
->>>>>>> daed4fa (no prb with normal commands)
+static void	exec_child(char *command, char *builtin, char **av, t_big *datas)
 {
 	int		ret_status;
 	int		fd[2];
@@ -39,7 +35,7 @@ static void	exec_child(char *cmd, char *builtin, char **av, t_big *datas)
 		if (builtin)
 			exec_built_in(builtin, av, datas);
 		else
-			exec_binary(cmd, av);
+			exec_binary(command, av);
 		exit(0);
 	}
 	waitpid(pid1, &ret_status, 0);
@@ -53,14 +49,7 @@ static void	exec_child(char *cmd, char *builtin, char **av, t_big *datas)
 void		exec_piped_cmd(char *cmd, char *builtin, char **av, t_big *datas)
 {
 	datas->flag_pipe = 1;
-	datas->flag_left_bracket = 0;
 	datas->flag_bracket = 0;
+	datas->flag_left_bracket = 0;
 	exec_child(cmd, builtin, av, datas);
-	//if (datas->flag_bracket)
-	//{
-	//	print_std_fd(datas->fd, datas->fd_out);
-	//	ft_putstr_fd("\n", datas->fd);
-	//	close(datas->fd_out);
-	//	datas->fd_out = STDOUT_FILENO;
-	//}
 }
