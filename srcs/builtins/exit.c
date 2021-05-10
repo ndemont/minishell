37426a6e8040ff6ba6c9143d05ce193e92f9298e
Nastasia Demont <nastasia@demont.me>
2021-05-10 15:45:37 +0200

    Correct invalid read for ft_exit: :

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 15:00:25 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/06 11:31:48 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/05/10 15:43:57 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int		ft_exit(char **av, t_big *datas)
 {
-	free_datas(datas);
 	write(0, "exit\n", 5);
-	if (av && av[1])
+	if (av && *av && av[1])
 	{
 		tcaps.ret = 1;
 		printf("minishellrose: exit: %s: numeric argument required\n", av[1]);
 	}
+	free_datas(datas);
 	exit(1);
 	return (tcaps.ret);
 }
