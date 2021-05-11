6670547ea280601a3e0d72aa1fa70bb15ca594cc
Nastasia DEMONT <ndemont@e2r2p18.42.fr>
2021-05-11 11:21:04 +0200

    Correct segfault for error in redirection

--- srcs/grammar/semicolon.c
+++ srcs/grammar/semicolon.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/15 14:04:11 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/11 11:19:20 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/05/11 11:20:35 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static void	exec_child(char *command, char **argv, t_big *datas)
 void		exec_semicolon_cmd(char *cmd, char **av, int is_builtin, t_big *datas)
 {
 	datas->flag_pipe = 0;
-	if (datas->flag_bracket)
+	if (datas->flag_bracket && datas->redirection_arg)
 		ft_putstr_fd(*datas->redirection_arg, datas->fd);
 	datas->flag_bracket = 0;
 	datas->flag_left_bracket = 0;
