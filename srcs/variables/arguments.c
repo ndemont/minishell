d67f86df3e4bac3f47ef56efbac09c9684edab78
Nastasia Demont <nastasia@demont.me>
2021-05-16 20:08:01 +0200

    Norme arguments in variable

--- srcs/variables/arguments.c
+++ srcs/variables/arguments.c
@@ -6,13 +6,13 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 22:16:02 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/14 12:47:35 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/05/16 19:12:20 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int		count_args(char **arg, t_big *datas)
+static int	count_args(char **arg, t_big *datas)
 {
 	int count;
 	int i;
@@ -27,29 +27,38 @@ static int		count_args(char **arg, t_big *datas)
 	return (count);
 }
 
-static char		**tab_join(char **tab1, t_big *datas)
+static char	**init_tab(char **tab1, t_big *datas)
 {
-	int		j;
-	int		i;
 	int		count;
 	char	**new;
 
-	i = 0;
-	j = 0;
 	count = count_args(tab1, datas);
-	if (!(new = (char **)malloc(sizeof(char *) * (count + 1))))
+	new = (char **)malloc(sizeof(char *) * (count + 1));
+	if (!(new))
 		return (printcc_stderr(0, strerror(errno), 0));
 	new[count] = 0;
-	while (tab1 && tab1[i])
-	{
+	return (new);
+}
+
+static char	**tab_join(char **tab1, t_big *datas)
+{
+	int		j;
+	int		i;
+	char	**new;
+
+	i = -1;
+	j = 0;
+	new = init_tab(tab1, datas);
+	if (!new)
+		return (0);
+	while (tab1 && tab1[++i])
 		new[i] = tab1[i];
-		i++;
-	}
 	if (tab1)
 		free(tab1);
-	while (i < count)
+	while (datas->redirection_arg[j])
 	{
-		if (!(new[i] = ft_strdup(datas->redirection_arg[j])))
+		new[i] = ft_strdup(datas->redirection_arg[j]);
+		if (!(new[i]))
 			return (printcc_stderr(0, strerror(errno), 0));
 		i++;
 		j++;
@@ -57,7 +66,7 @@ static char		**tab_join(char **tab1, t_big *datas)
 	return (new);
 }
 
-char	**get_arguments(t_node *root, t_big *datas)
+char		**get_arguments(t_node *root, t_big *datas)
 {
 	if (datas->redirection_arg)
 	{
