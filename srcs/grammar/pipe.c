75b2ac833c75506b884166fb04555713fb2492a1
Guillaume PETIT <gpetit@e3r4p14.42.fr>
2021-05-18 14:49:39 +0200

    Calling pipes now close open file descriptors

--- srcs/grammar/pipe.c
+++ srcs/grammar/pipe.c
@@ -17,6 +17,12 @@ void	actualize_return_status(int ret_status)
 	tcaps.ret = WEXITSTATUS(ret_status);
 }
 
+void	close_pipes(int *fd)
+{
+	close(fd[0]);
+	close(fd[1]);
+}
+
 static void	exec_child(char *command, char *builtin, char **av, t_big *datas)
 {
 	int		ret_status;
@@ -32,7 +38,7 @@ static void	exec_child(char *command, char *builtin, char **av, t_big *datas)
 		dup2(datas->fd, STDIN_FILENO);
 		close(datas->fd);
 		dup2(fd[1], STDOUT_FILENO);
-		close(fd[0]);
+		close_pipes(fd);
 		if (builtin)
 			ret_status = exec_built_in(builtin, av, datas);
 		else
@@ -43,7 +49,7 @@ static void	exec_child(char *command, char *builtin, char **av, t_big *datas)
 	waitpid(pid1, &ret_status, 0);
 	actualize_return_status(ret_status);
 	dup2(fd[0], datas->fd);
-	close(fd[1]);
+	close_pipes(fd);
 }
 
 void	exec_piped_cmd(char *cmd, char *builtin, char **av, t_big *datas)
