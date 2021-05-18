4f48b3186c57a6203c7cf300beb249833b6c41ff
Nastasia Demont <nastasia@demont.me>
2021-05-18 19:55:33 +0200

    Norme free

---
+++ srcs/free/free_datas.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_datas.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/18 19:36:04 by ndemont           #+#    #+#             */
+/*   Updated: 2021/05/18 19:36:13 by ndemont          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int		free_datas(t_big *datas)
+{
+	if (datas)
+	{
+		if (datas->redirection_arg)
+			free_double(datas->redirection_arg);
+		if (datas && datas->file_name)
+			free(datas->file_name);
+		if (datas->env)
+			free_list(datas->env);
+		if (datas->export)
+			free_list(datas->export);
+		if (datas->hidden)
+			free_list(datas->hidden);
+		if (datas->history)
+			free_history(datas->history);
+		if (datas->root)
+			free_tree(datas->root);
+		if (datas->input)
+			free(datas->input);
+		if (datas->browse)
+			free(datas->browse);
+	}
+	return (1);
+}
