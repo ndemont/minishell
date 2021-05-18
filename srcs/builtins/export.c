dac3b86b14804a26e29ab845a2e0e80a4f0e4d92
Guillaume PETIT <gpetit@e3r4p14.42.fr>
2021-05-18 18:25:47 +0200

    retour de export en cours

--- srcs/builtins/export.c
+++ srcs/builtins/export.c
@@ -12,32 +12,45 @@
 
 #include "minishell.h"
 
-void	non_conform_argument(char *arg)
+int	non_conform_argument(char *arg)
 {
 	static char	error_str[] = "' not a valid identifier";
 	char		*identifier;
 
 	identifier = get_identifier(arg);
+	if (!identifier)
+		return (ERROR);
 	identifier = ft_realloc(identifier, ft_strlen(identifier) + \
 	ft_strlen(error_str) + 1);
+	if (!identifier)
+		return (printi_stderr(0, strerror(errno), 0));
 	ft_strlcat(identifier, error_str, ft_strlen(identifier) + \
 	ft_strlen(error_str) + 1);
 	printi_stderr("export: '", identifier, 1);
 	free(identifier);
 	tcaps.ret = 1;
+	return (SUCCESS);
 }
 
-void	argument_without_equal(char *arg, t_big *datas)
+int	argument_without_equal(char *arg, t_big *datas)
 {
+	int	ret;
+
+	ret = SUCCESS;
 	if (check_duplicate(*datas->hidden, arg) && \
 	!check_duplicate(*datas->export, arg))
-		add_hidden_to_env_export(arg, datas);
+		ret = add_hidden_to_env_export(arg, datas);
 	else if (!check_duplicate(*datas->hidden, arg) && \
 	!check_duplicate(*datas->export, arg))
-		add_hidden_add_export(arg, datas);
+		ret = add_hidden_add_export(arg, datas);
+	if (!ret)
+		return (ERROR);
+	else
+		return (SUCCESS);
+	
 }
 
-void	argument_with_equal(char *arg, t_big *datas)
+int	argument_with_equal(char *arg, t_big *datas)
 {
 	if (!check_duplicate(*datas->hidden, arg))
 		add_hidden_add_export_add_env(arg, datas);
@@ -53,21 +66,35 @@ void	argument_with_equal(char *arg, t_big *datas)
 		else if (!check_duplicate(*datas->export, arg))
 			add_hidden_to_env_export(arg, datas);
 	}
+	return (SUCCESS);
 }
 
-void	treat_argument(char *arg, t_big *datas)
+int	treat_argument(char *arg, t_big *datas)
 {
-	if (!check_arg_conformity(arg))
-		non_conform_argument(arg);
-	else if (!ft_strchr(arg, '='))
-		argument_without_equal(arg, datas);
-	else if (ft_strchr(arg, '='))
-		argument_with_equal(arg, datas);
+	int	ret;
+	int ret2;
+	int	ret_final;
+
+	ret_final = SUCCESS;
+	ret = check_arg_conformity(arg);
+	ret2 = ft_strchr(arg, '=');
+	if (ret == BUILT_IN_FAILURE || ret2 == BUILT_IN_FAILURE)
+		return (BUILT_IN_FAILURE);
+	if (!ret)
+		ret_final = non_conform_argument(arg);
+	else if (!ret2)
+		ret_final = argument_without_equal(arg, datas);
+	else if (ret2 > 0)
+		ret_final = argument_with_equal(arg, datas);
+	if (!ret_final)
+		return (ERROR);
+	return (SUCCESS);
 }
 
 int		ft_export(char **arg, t_big *datas)
 {
-	int		i;
+	int	i;
+	int	ret;
 
 	i = 1;
 	if (!arg[i])
@@ -75,7 +102,11 @@ int		ft_export(char **arg, t_big *datas)
 	else
 	{
 		while (arg[i])
-			treat_argument(arg[i++], datas);
+		{
+			ret = treat_argument(arg[i++], datas);
+			if (ret == BUILT_IN_FAILURE)
+				return (BUILT_IN_FAILURE);
+		}
 	}
 	*datas->export = ft_lst_sort(*datas->export, &cmp_list);
 	return (tcaps.ret);
