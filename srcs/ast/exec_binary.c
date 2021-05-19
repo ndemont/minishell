f05e4d5e6610440a1f7082b6f103a9ddd47acfc5
Guillaume PETIT <gpetit@e3r1p4.42.fr>
2021-05-19 13:03:44 +0200

    exec binary almost normed

--- srcs/ast/exec_binary.c
+++ srcs/ast/exec_binary.c
@@ -12,63 +12,72 @@
 
 #include "minishell.h"
 
+void	free_two_elems(char **tmp, char ***paths)
+{
+	clean_free(tmp);
+	free_double(*paths);
+}
+
+char	**path_array2(char **tmp, char ***paths)
+{
+	int		k;
+	char	**binaries;
+
+	k = 0;
+	binaries = (char **)malloc(sizeof(char *) * (ft_dbstrlen(*paths) + 1));
+	if (!(*tmp) || !binaries)
+	{
+		free_two_elems(tmp, paths);
+		return (printcc_stderr(0, strerror(errno), 0));
+	}
+	while ((*paths)[k])
+	{
+		binaries[k] = ft_strjoin((*paths)[k], *tmp);
+		if (!binaries[k])
+		{
+			free_two_elems(tmp, paths);
+			return (printcc_stderr(0, strerror(errno), 0));
+		}
+		k++;
+	}
+	binaries[k] = 0;
+	free(*tmp);
+	free_double(*paths);
+	return (binaries);
+}
+
 char	**path_array(char *command, t_list *env)
 {
 	char	*tmp;
 	char	**paths;
 	char	**binaries;
-	int		k;
 
-	k = 0;
 	paths = NULL;
 	while (env && ft_strcmp(((t_var *)(env->content))->var, "PATH"))
 		env = env->next;
 	if (!env)
-	{
-		perror("PATH is invalid ");
-		exit(0); //replace by custom exit fct that deallocate everything
-	}
+		return (printcc_stderr(0, "PATH is invalid", 1));
 	if (!ft_strcmp(((t_var *)(env->content))->var, "PATH"))
 		paths = ft_split(((t_var *)(env->content))->value, ':');
 	if (!paths)
-	{
-		perror("ft_split failed ");
-		exit(0); //replace by custom exit fct that deallocate everything
-	}
+		return (printcc_stderr(0, strerror(errno), 1));
 	tmp = ft_strjoin("/", command);
-	binaries = (char **)malloc(sizeof(char *) * (ft_dbstrlen(paths) + 1));
-	while (paths[k])
-	{
-		binaries[k] = ft_strjoin(paths[k], tmp);
-		if (!binaries[k])
-		{
-			printf("k = %i\n", k);
-			perror("strjoin_failed");
-			exit(0); //replace by custom exit fct that deallocate everything
-		}
-		k++;
-	}
-	binaries[k] = 0;
-	free(tmp);
-	free_double(paths);
+	binaries = path_array2(&tmp, &paths);
 	return (binaries);
 }
 
 char	**mono_array(char *str)
 {
-	char **cmd;
+	char	**cmd;
 
-	cmd = (char **)malloc(sizeof(char*) * 2);
+	cmd = (char **)malloc(sizeof(char *) * 2);
 	if (!cmd)
-	{
-		perror("Malloc failed in mono_array ");
-		exit(0); //replace by custom exit fct
-	}
+		return (printcc_stderr(0, strerror(errno), 1));
 	cmd[0] = ft_strdup(str);
 	if (!cmd[0])
 	{
-		perror("Malloc failed in mono_array2 ");
-		exit(0);
+		free(cmd);
+		return (printcc_stderr(0, strerror(errno), 1));
 	}
 	cmd[1] = 0;
 	return (cmd);
@@ -85,6 +94,30 @@ char	**build_array(char *command, t_list *env)
 	return (cmd);
 }
 
+char	*env_line(t_list *lst, char ***env)
+{
+	char	*line;
+	int		size;
+
+	line = ft_strjoin(((t_var *)(lst->content))->var, "=");
+	if (!line)
+	{
+		free_double(*env);
+		printi_stderr(0, strerror(errno), 0);
+		return (ERROR);
+	}
+	size = ft_strlen(line) + ft_strlen(((t_var *)(lst->content))->value) + 1;
+	line = ft_realloc(line, size);
+	if (!line)
+	{
+		free_double(*env);
+		printi_stderr(0, strerror(errno), 0);
+		return (ERROR);
+	}
+	ft_strlcat(line, ((t_var *)(lst->content))->value, size);
+	return (line);
+}
+
 char	**build_array_env(t_list *lst)
 {
 	int		size;
@@ -95,11 +128,13 @@ char	**build_array_env(t_list *lst)
 	i = 0;
 	size = ft_lstsize(lst);
 	env = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!env)
+		return (printcc_stderr(0, strerror(errno), 0));
 	while (lst)
 	{
-		line = ft_strjoin(((t_var *)(lst->content))->var, "=");
-		line = ft_realloc(line, ft_strlen(line) + ft_strlen(((t_var *)(lst->content))->value) + 1);
-		ft_strlcat(line, ((t_var *)(lst->content))->value, (ft_strlen(line) + ft_strlen(((t_var *)(lst->content))->value) + 1));
+		line = env_line(lst, &env);
+		if (!line)
+			return (ERROR);
 		env[i] = line;
 		i++;
 		lst = lst->next;
@@ -108,7 +143,7 @@ char	**build_array_env(t_list *lst)
 	return (env);
 }
 
-int		exec_binary(char *command, char **argv, t_big *datas)
+int	exec_binary(char *command, char **argv, t_big *datas)
 {
 	char	**cmd;
 	char	**env;
@@ -117,18 +152,19 @@ int		exec_binary(char *command, char **argv, t_big *datas)
 
 	cmd = build_array(command, *datas->env);
 	env = build_array_env(*datas->env);
+	if (!cmd || !env)
+		return (ERR);
 	k = 0;
-	while (cmd[k] && ((ret = execve(cmd[k], argv, env)) == -1))
-		k++;
+	ret = execve(cmd[k], argv, env);
+	while (cmd[k] && ret == -1)
+		ret = execve(cmd[++k], argv, env);
 	if (ret == -1)
 	{
 		if (errno == 2)
 			tcaps.ret = RET_ERROR;
 		else if (errno == 13)
 			tcaps.ret = 126;
-		ft_putstr_fd("minishellrose: ", STDERR_FILENO);
-		ft_putstr_fd(argv[0], STDERR_FILENO);
-		ft_putstr_fd(": command not found\n", STDERR_FILENO);
+		printi_stderr(argv[0], ": command not found\n", 1);
 	}
 	free_double(cmd);
 	free_double(env);
