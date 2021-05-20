b9fda6a8d70f28f40d0e6d2b45d6df1c1dc1199f
Nastasia Demont <nastasia@demont.me>
2021-05-20 22:54:42 +0200

    Manage empty var with command

--- srcs/variables/update_arguments.c
+++ srcs/variables/update_arguments.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 22:16:02 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/19 11:28:10 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/05/20 22:50:04 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,25 @@
 static int	count_args(char **arg, t_big *datas)
 {
 	int	count;
+	int j;
 	int	i;
 
 	i = 0;
+	j = 0;
 	count = 0;
 	if (datas->redirection_arg)
 	{
-		while (datas->redirection_arg[count])
-			count++;
+		while (datas->redirection_arg[count] && datas->redirection_arg[count][0])
+			j++;
+		count++;
 	}
 	while (arg && arg[i])
+	{
+		if (arg[i] && arg[i][0])
+			j++;
 		i++;
-	count += i;
-	return (count);
+	}
+	return (j);
 }
 
 static char	**init_tab(char **tab1, t_big *datas)
@@ -49,34 +55,44 @@ static char	**tab_join(char **tab1, t_big *datas)
 	int		i;
 	char	**new;
 
-	i = -1;
+	i = 0;
 	j = 0;
 	new = init_tab(tab1, datas);
 	if (!new)
 		return (0);
-	while (tab1 && tab1[++i])
-		new[i] = tab1[i];
+	while (tab1 && tab1[i])
+	{
+		if (tab1[i][0])
+		{
+			new[j] = tab1[i];
+			j++;
+		}
+		//else
+		//	free(tab1[i]);
+		i++;
+	}
 	if (tab1)
 		free(tab1);
-	while (datas->redirection_arg[j])
+	i = 0;
+	while (datas->redirection_arg && datas->redirection_arg[i])
 	{
-		new[i] = ft_strdup(datas->redirection_arg[j]);
-		if (!(new[i]))
-			return (printcc_stderr(0, strerror(errno), 0));
+		if (datas->redirection_arg[i][0])
+		{
+			new[j] = ft_strdup(datas->redirection_arg[i]);
+			if (!(new[j]))
+				return (printcc_stderr(0, strerror(errno), 0));
+			j++;
+		}
 		i++;
-		j++;
 	}
 	return (new);
 }
 
 char	**get_arguments(t_node *root, t_big *datas)
 {
-	if (datas->redirection_arg)
-	{
-		root->arg = tab_join(root->arg, datas);
-		if (!(root->arg))
-			return (printcc_stderr(0, strerror(errno), 0));
-		datas->redirection_arg = 0;
-	}
+	root->arg = tab_join(root->arg, datas);
+	if (!(root->arg))
+		return (printcc_stderr(0, strerror(errno), 0));
+	datas->redirection_arg = 0;
 	return (root->arg);
 }
