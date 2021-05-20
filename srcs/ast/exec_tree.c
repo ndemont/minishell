9b685bbe907d74cc63828a7b6583078f3c54ef74
Nastasia Demont <nastasia@demont.me>
2021-05-20 22:54:55 +0200

    Manage empty var without command

--- srcs/ast/exec_tree.c
+++ srcs/ast/exec_tree.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 23:40:03 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/20 19:21:43 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/05/20 22:54:07 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,22 +38,22 @@ void	execute_tree(t_node *root, int parent, t_big *datas, int side)
 			execute_tree(root->left, root->type, datas, 1);
 		if (root->right)
 			execute_tree(root->right, root->type, datas, 2);
+		replace_variable(root, datas);
 		if (parent != 2 && parent != 3 && parent != 4 && !root->type)
 		{
 			if (parent == 1)
 				g_tcaps.ret = 0;
 			root->arg = get_arguments(root, datas);
-			replace_variable(root, datas);
 		}
-		if (parent == 0)
+		if (parent == 0 && root->arg[0])
 			no_parent(root, parent, datas, side);
-		if (parent == 1)
+		if (parent == 1 && root->arg[0])
 			pipe_parent(root, parent, datas, side);
-		if (parent == 2 || parent == 3)
+		if ((parent == 2 || parent == 3) && root->arg[0])
 			right_redir_parent(root, parent, datas, side);
-		if (parent == 4)
+		if (parent == 4 && root->arg[0])
 			left_redir_parent(root, parent, datas, side);
-		if (parent == 5)
+		if (parent == 5 && root->arg[0])
 			semicolon_parent(root, parent, datas, side);
 	}
 }
