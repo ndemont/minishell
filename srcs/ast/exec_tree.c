480e960fc45b449d045d6c3216b1acabe70a717a
ndemont <nastasia@demont.me>
2021-05-21 11:43:12 +0200

    Correct pb de fillle

--- srcs/ast/exec_tree.c
+++ srcs/ast/exec_tree.c
@@ -6,7 +6,7 @@
 /*   By: ndemont <ndemont@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 23:40:03 by ndemont           #+#    #+#             */
-/*   Updated: 2021/05/21 11:21:13 by ndemont          ###   ########.fr       */
+/*   Updated: 2021/05/21 11:42:41 by ndemont          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,8 @@ void	execute_tree(t_node *root, int parent, t_big *datas, int side)
 			execute_tree(root->left, root->type, datas, 1);
 		if (root->right)
 			execute_tree(root->right, root->type, datas, 2);
+		if (datas->quit)
+			return ;
 		replace_variable(root, datas);
 		if (parent != 2 && parent != 3 && parent != 4 && !root->type)
 		{
@@ -66,7 +68,7 @@ void	executions(t_big *datas)
 	datas->flag_pipe = 0;
 	datas->fd = dup(STDIN_FILENO);
 	execute_tree(datas->root, 0, datas, 0);
-	if (!g_tcaps.exit)
+	if (!g_tcaps.exit || datas->quit)
 		return ;
 	if (datas->flag_pipe)
 		print_std(datas->fd);
